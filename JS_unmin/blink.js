07b4ad5a8cb1cf10de284314bbf07a60109d0fa8
renn999 <renn999@88f66ee3-ab8e-9ca8-0e6b-7893c2cead9a>
2010-10-26 02:25:31 +0000

    JS_unmin dummy clean
    git-svn-id: http://ccns-bbs-web-system.googlecode.com/svn/trunk@6 88f66ee3-ab8e-9ca8-0e6b-7893c2cead9a

--- JS_unmin/blink.js
+++ JS_unmin/blink.js
@@ -15,5 +15,3 @@ function blinkOut() {
     });
     setTimeout("blinkIn()", 1000);
 }
-$(function(){blinkIn();});function blinkIn(){$(".sbk").each(function(){$(this).css("color",$(this).css("background-color"));});setTimeout("blinkOut()",1000);}
-function blinkOut(){$(".sbk").each(function(){$(this).css("color","");});setTimeout("blinkIn()",1000);}
